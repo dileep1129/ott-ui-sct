5a1f0587c7daf0150d9f73d72e6c1ec298a8bf67
dileep1129 <dileepkumarpechetti@gmail.com>
2025-02-27 10:54:13 +0530

    removed code from app.jsx

--- ottproject/ott_ui/src/App.jsx
+++ ottproject/ott_ui/src/App.jsx
@@ -9,43 +9,7 @@ function App() {
   
 return (
   <div>
-  {/* <NavBar />
-   <form onSubmit={handleSubmit}>
-        <InputField
-            label="Email"
-            type="email"
-            name="email"
-            placeholder="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-        />
-        <InputField
-            label="Password"
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
-        />
-        <InputField
-            label="Username"
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            minLength={3}
-            maxLength={20}
-        />
-        <InputField
-            label="Age"
-            type="number"
-            name="age"
-            value={formData.age}
-            onChange={handleChange}
-        />
-        <CustomButton label="Submit Form" type="submit" color="success" />
-    </form> */}
+ 
     <AppRoutes />
   </div>
    
